c32c5fc920a7b938d5c11c1160ef75909b04fdcf
Paul Kiddle <NeonPaul@users.noreply.github.com>
2016-10-09 23:10:55 +0100

    Comments fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,8 +23,9 @@ sudo apt-get install -y -qq mysql-server-5.5 mysql-client
 
 #mysql_secure_installation
 
+# configure mysql
 mysql -u $DB_USER -p$DB_PASS shack < /home/vagrant/shack.sql
 
-install node
+# install node
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y -qq nodejs
